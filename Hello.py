b2413ac4ef2975ac457d6c66bb1a0b76b46fa542
ayshajaved <ayshajavd@gmail.com>
2025-01-02 00:16:53 +0500

    Language

--- Hello.py
+++ Hello.py
@@ -2,3 +2,5 @@ if __name__ == "__main__":
     print("Hello world")
     print("This is the python Repository")
     print("I will make the highest contributions")
+    for i in range(3):
+        print("Python is the best language")
